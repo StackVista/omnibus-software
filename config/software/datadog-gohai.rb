73ba5711e5cda5e233c67f2060f68ae9a4f28c50
Wytze Hazenberg <wytzehazenberg@gmail.com>
2017-06-08 12:48:16 +0200

    merge upstream gohai

--- config/software/datadog-gohai.rb
+++ config/software/datadog-gohai.rb
@@ -1,35 +1,29 @@
 name "datadog-gohai"
 default_version "last-stable"
 
-always_build true
-
 env = {
-  "GOPATH" => "#{Omnibus::Config.cache_dir}/src/#{name}",
+  "GOROOT" => "/usr/local/go",
+  "GOPATH" => "/var/cache/omnibus/src/datadog-gohai"
 }
 
-if ohai["platform_family"] == "mac_os_x"
+if ohai['platform_family'] == 'mac_os_x'
+  env.delete "GOROOT"
   gobin = "/usr/local/bin/go"
-elsif ohai["platform"] == "windows"
-  gobin = "C:/Go/bin/go"
 else
-  env["GOROOT"] = "/usr/local/go"
   gobin = "/usr/local/go/bin/go"
 end
 
 build do
-  ship_license "https://raw.githubusercontent.com/DataDog/gohai/#{version}/LICENSE"
-  ship_license "https://raw.githubusercontent.com/DataDog/gohai/#{version}/THIRD_PARTY_LICENSES.md"
-  # Checkout gohai's deps
-  command "#{gobin} get -u github.com/shirou/gopsutil", :env => env
-  command "git checkout v2.0.0", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-gohai/src/github.com/shirou/gopsutil"
-  command "#{gobin} get -u github.com/cihub/seelog", :env => env
-  command "git checkout v2.6", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-gohai/src/github.com/cihub/seelog"
-  # Windows depends on the registry, go get that.
-  if ohai["platform"] == "windows"
-    command "#{gobin} get golang.org/x/sys/windows/registry", :env => env
-  end
-  # Checkout and build gohai
-  command "#{gobin} get -d github.com/DataDog/gohai", :env => env # No need to pull latest from remote with `-u` here since the next command checks out and pulls latest
-  command "git checkout #{version} && git pull", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-gohai/src/github.com/DataDog/gohai"
-  command "cd #{env['GOPATH']}/src/github.com/DataDog/gohai && #{gobin} run make.go #{gobin} && mv gohai #{install_dir}/bin/gohai", :env => env
+   ship_license "https://raw.githubusercontent.com/DataDog/gohai/#{version}/LICENSE"
+   ship_license "https://raw.githubusercontent.com/DataDog/gohai/#{version}/THIRD_PARTY_LICENSES.md"
+   # Go get gohai
+   command "#{gobin} get -d -u github.com/DataDog/gohai", :env => env
+   # Checkout gohai's deps
+   command "#{gobin} get -u github.com/shirou/gopsutil", :env => env
+   command "git checkout v2.0.0", :env => env, :cwd => "/var/cache/omnibus/src/datadog-gohai/src/github.com/shirou/gopsutil"
+   command "#{gobin} get -u github.com/cihub/seelog", :env => env
+   command "git checkout v2.6", :env => env, :cwd => "/var/cache/omnibus/src/datadog-gohai/src/github.com/cihub/seelog"
+   # Checkout and build gohai
+   command "git checkout #{version} && git pull", :env => env, :cwd => "/var/cache/omnibus/src/datadog-gohai/src/github.com/DataDog/gohai"
+   command "cd $GOPATH/src/github.com/DataDog/gohai && #{gobin} run make.go '#{gobin}' && mv gohai #{install_dir}/bin/gohai", :env => env
 end
