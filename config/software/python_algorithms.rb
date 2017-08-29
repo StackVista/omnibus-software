8636bd74c5e7b97ea29dc61b8abf4b2d5b47f798
ktatarnikov <ktatarnikov@stackstate.com>
2017-08-29 19:09:35 +0200

    license statement is not used

--- config/software/python_algorithms.rb
+++ config/software/python_algorithms.rb
@@ -5,6 +5,5 @@ dependency "python"
 dependency "pip"
 
 build do
-  license "https://url.to.my/LICENSE.txt"
   command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
 end
