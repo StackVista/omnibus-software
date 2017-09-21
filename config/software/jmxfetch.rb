bae6537389e7c3eecd5817c893e55aab6dfd3489
Bram Schuur <bschuur@stackstate.com>
2017-09-21 11:43:22 +0200

    Take jmxfetch from S3

--- config/software/jmxfetch.rb
+++ config/software/jmxfetch.rb
@@ -11,24 +11,8 @@ else
   end
 end
 
-version "0.12.0" do
-  source md5: "2a04e4f02de90b7bbd94e581afb73c8f"
-end
-
-version "0.13.0" do
-  source sha256: "741a74e8aed6bcfd726399bf0e5094bb622f7a046004a57e3e39e11f53f72aa0"
-end
-
-version "0.13.1" do
-  source sha256: "7dcd8370c500b68e53465e6cc4b578302919cd272c12d2a32b4766eb41c03339"
-end
-
-version "0.14.0" do
-  source sha256: "e4fa8ce43f2af3e6ee9ddbcdb7c34a84646484374c8e19718ce507fdee96ec55"
-end
-
 version "0.15.0" do
-  source sha256: "fcdcb204203c83409337ef00f34a09092b4314e8ce1ba094595b7c0c153a5678"
+  source sha256: "f735057a6e2d3cc15a0be82a1223127f73afb5857ffcf3a6d154d5c5df198ed9"
 end
 
 source :url => "https://s3-eu-west-1.amazonaws.com/sts-jmxfetch/jmxfetch-#{version}-jar-with-dependencies.jar"
